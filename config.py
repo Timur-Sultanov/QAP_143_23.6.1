07d4b8ec1e1dbdf9218628ca25b05cb534251736
Timur-Sultanov <SultanovT.R@yandex.ru>
2023-12-19 00:35:39 +0300

    completed

---
+++ config.py
@@ -0,0 +1,10 @@
+TOKEN = "6376748168:AAG-ke_AnssvUqQMVBEG1_pjjwukMOsmVw4"
+keys = {
+    "рубль": "RUB",
+    "доллар": "USD",
+    "евро": "EUR",
+    "песо": "MXN",
+    "лари": "GEL",
+    "дирхам": "AED",
+
+}
